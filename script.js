f205ca76b9762d7c216a41df22c66eefdacf88de
McKenna Frazier <frazier22028@lcsd2kids.org>
2021-05-10 11:23:16 -0600

    I have added to question on how many grid squares they want and now have also added the button to clear it.

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 const gridContainer = document.querySelector("#grid-container");
-let numOfTimes = 256;
+const root = document.documentElement;
+// object.onclick = function(){myScript};
+
+let numOfTimes = 16;
+let autos = "";
 for(let i =0;i<numOfTimes;i++) {
-    const singleCell  = document.createElement("div");
-    singleCell.textContent = "McKenna";
-    singleCell.style.color = "black";
-    singleCell.classList.add("bg-red")
-    singleCell.addEventListener("mouseover", () => {
-        singleCell.classList.add("mouse-over");
-    });
-    gridContainer.appendChild(singleCell);
+    autos += " auto";
 }
+document.querySelector("#btn").addEventListener('click', () => {
+    numOfTimes = prompt("Question ");
+    gridContainer.innerHTML = "";
+    buildGridFunction();
+});
+console.log(autos)
+root.style.setProperty('--cols',autos);
+
+function buildGridFunction(){
+
+
+for(let j =0;j<numOfTimes;j++) {
+    const row  = document.createElement("div");
+    for(let i =0;i<numOfTimes;i++) {
+        const singleCell  = document.createElement("div");
+        singleCell.innerHTML = "&nbsp;";
+        singleCell.style.color = "black";
+        singleCell.classList.add("grid-item")
+        singleCell.addEventListener("mouseover", () => {
+            singleCell.classList.add("mouse-over");
+        });
+        row.appendChild(singleCell);
+    }
+    gridContainer.appendChild(row);
+}
+}
+
+buildGridFunction();
